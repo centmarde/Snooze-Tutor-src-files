6a535a7d0960772290ee1e38c329e24ee5ed8b1d
whitelotus19 <centmarde.campado@gmail.com>
2024-04-26 13:16:20 +0800

    added view comments

--- js/auth/home.js
+++ js/auth/home.js
@@ -162,7 +162,7 @@ form_item.onsubmit = async (e) => {
       .from("questions")
       .insert([
         {
-          tittle: formData.get("tittle"),
+          title: formData.get("title"),
           question_text: formData.get("question"),
           answer_text: formData.get("answer"),
           user_id: userId,
@@ -204,96 +204,136 @@ form_item.onsubmit = async (e) => {
 
 //get questions
 async function getQuestions(keyword = "") {
-  let { data: questions, error } = await supabase
+  let { data: questions, error: questionError } = await supabase
     .from("questions")
     .select("*,profiles(*)")
     .or(
       "question_text.ilike.%" +
           keyword +
-          "%, tittle.ilike.%" +
+          "%, title.ilike.%" +
           keyword +
           "%"
-  );
- 
-    questions.sort(() => Math.random() - 0.5);
-    //CHILLI SPICY COM-SCI LORDS BABY!
-  
+    );
+
+  let { data: comments, error: commentError } = await supabase
+    .from("comments")
+    .select("*,profiles(*)");
+
+  // Initialize an object to store comments for each question
+  let questionComments = {};
+  comments.forEach(comment => {
+    const questionId = comment.question_id;
+    if (!questionComments[questionId]) {
+      questionComments[questionId] = [];
+    }
+    questionComments[questionId].push(comment);
+  });
+
+  questions.sort(() => Math.random() - 0.5);
+
   let questionContainer = "";
-  console.log(questionContainer);
 
   questions.forEach((data, index) => {
-    
     const imagepath = data.profiles.image_path;
     const username = data.profiles.username;
     const likes = data.profiles.likes;
 
-    questionContainer += `<div class="col-lg-6 col-md-12 col-sm-12 justify-content-center mb-3">
-    <div class="card  hiddenAnimate2" ">
-      <div class="card"  background-color:#e3deeb;">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-4 col-md-12">
-              <img
-                src="${itemsImageUrl + imagepath}"
-                class="block my-2 border border-dark border-2 rounded-circle"
-                width="80px"
-                height="80px"
-              />
-            </div>
-            <div class="col-8 col-md-12">
-              <div>
-                <p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18"  height="18" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                <path  d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                </svg>
-
-                <b class ="me-1">IGN:</b> ${username}</p>
-
-                <p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
-                <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
-                </svg>
-            
-                <b class ="me-1">Likes:</b> ${likes}</p>
+    let commentHtml = "";
+    if (questionComments[data.id]) {
+      questionComments[data.id].forEach(comment => {
+        const commentImagePath = comment.profiles.image_path;
+        const commentUsername = comment.profiles.username;
+        commentHtml += `
+          <div class="col-2 my-1">
+            <img
+              src="${itemsImageUrl + commentImagePath}"
+              class="block my-2 border border-dark border-2 rounded-circle d-flex align-items-center"
+              width="40px"
+              height="40px"
+            />
+          </div>
+          <div class="col-10 my-1">
+            <div class="card">
+              <div class="card-body p-0">
+                <b>${commentUsername}</b>
+                <p>${comment.comment_text}</p>
               </div>
             </div>
-          </div>
-          <h2 class="card-title text-center">${data.tittle}</h2>
-          <p class="card-text text-center">
-          ${data.question_text}
-          </p>
-          <div id="textContainer${index}" class="d-grid gap-2 d-none">
+          </div>`;
+      });
+    }
+
+    questionContainer += `
+      <div class="col-lg-6 col-md-12 col-sm-12 justify-content-center mb-3">
+        <div class="card hiddenAnimate2" data-id="${data.id}">
+          <div class="card" style="background-color:#e3deeb;">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-4 col-md-12">
+                  <img
+                    src="${itemsImageUrl + imagepath}"
+                    class="block my-2 border border-dark border-2 rounded-circle"
+                    width="80px"
+                    height="80px"
+                  />
+                </div>
+                <div class="col-8 col-md-12">
+                  <div>
+                    <p>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path  d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                      </svg>
+                      <b class="me-1">IGN:</b> ${username}</p>
+                    <p>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+                        <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+                      </svg>
+                      <b class="me-1">Likes:</b> ${likes}</p>
+                  </div>
+                </div>
+              </div>
+              <h2 class="card-title text-center">${data.title}</h2>
+              <p class="card-text text-center">${data.question_text}</p>
+              <div id="textContainer${index}" class="d-grid gap-2 d-none">
               <i>${data.answer_text}</i>
           </div>
           <div class="d-grid gap-2 mt-2" >
           <button type="button" id="showButton${index}" class="btn btn-dark"  style=" background-color:#2b1055;">Show Answer</button>
           </div>
-          
-              <div class="row" >
-                <div class="col mt-3"><h5 >Show Profile...</h5>
-              </div>
-              
+              <div class="row">
+                <div class="col mt-3">
+                  <h5>Show Profile...</h5>
+                </div>
                 <div class="col mt-3 d-flex justify-content-end">
-                <div class="d-flex me-3" data-bs-toggle="modal"
-                data-bs-target="#modal_heart">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                </svg>
-                <p class="ms-2">${data.heart}</p>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 16">
-                <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-                <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
-                </svg>
-                
+                  <div class="d-flex me-3" data-bs-toggle="modal" data-bs-target="#modal_heart">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                    </svg>
+                    <p class="ms-2">${data.heart}</p>
+                  </div>
+                  <div data-bs-toggle="modal" data-bs-target="#modal_comment">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 16">
+                      <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                      <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
+                    </svg>
+                  </div>
                 </div>
               </div>
-          
+              <div class="row">
+                <div class="d-grid gap-2">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="d-flex row" id="commentWrapper${index}"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <!-- modal for hearts -->
+
+      <!-- modal for hearts -->
     
   <div class="modal fade" id="modal_heart" tabindex="-1">
    <div class="modal-dialog modal-dialog-centered">
@@ -315,23 +355,129 @@ async function getQuestions(keyword = "") {
  </div>
 
   <!-- end modal for hearts -->
-  
-  
-  `;
+      <!-- modal for Comments -->
+    
+      <div class="modal fade" id="modal_comment" tabindex="-1">
+      <form id="form_comment">
+       <div class="modal-dialog modal-dialog-centered">
+         <div class="modal-content">
+           <div class="modal-header">
+             <h5 class="modal-title text-center">Add Comment:</h5>
+             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+           </div>
+           <div class="modal-body">
+           <div class="mb-3">
+           <label for="exampleFormControlTextarea1" class="form-label">Input:</label>
+           <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+         </div>
+           </div>
+           <div class="modal-footer">
+             <button type="button" id="modal_close_heart" class="btn" data-bs-dismiss="modal" style="background-color: #e00909; color: white;">Cancel</button>
+             <button type="button" data-id ="${data.id}" id="comment_add" class="btn" style="background-color: #2b1055; color: white;" >Save Comment</button>
+           </div>
+         </div>
+       </div>
+       </form>
+     </div>
+    
+      <!-- end modal for Comments -->
+      `;
+  });
 
-});
 
 
+  document.body.addEventListener("click", function (event) {
+    if (event.target.id === "comment_add") {
+      alert("waaaaaaaaaaaa");
+    }
+  });
 
-document.getElementById("indexContainer").innerHTML = questionContainer;
+  document.getElementById("indexContainer").innerHTML = questionContainer;
 
-for (let i = 0; i < questions.length; i++) {
+  for (let i = 0; i < questions.length; i++) {
     var showButton = document.getElementById(`showButton${i}`);
     showButton.onclick = function () {
         var textContainer = document.getElementById(`textContainer${i}`);
         textContainer.classList.remove("d-none");
     };
-}}
+}
+
+questions.forEach((data, index) => {
+  let commentWrapper = "";
+  let showMoreLink = "";
+  if (questionComments[data.id]) {
+    const firstComment = questionComments[data.id][0];
+    const remainingComments = questionComments[data.id].slice(1);
+    const imagepath = firstComment.profiles.image_path;
+    const username = firstComment.profiles.username;
+    commentWrapper += `
+      <div class="col-2 my-1">
+        <img
+          src="${itemsImageUrl + imagepath}"
+          class="block my-2 border border-dark border-2 rounded-circle d-flex align-items-center"
+          width="40px"
+          height="40px"
+        />
+      </div>
+      <div class="col-10 my-1">
+        <div class="card">
+          <div class="card-body p-0 ms-2">
+            <b>${username}</b>
+            <p>${firstComment.comment_text}</p>
+          </div>
+        </div>
+      </div>`;
+
+    if (remainingComments.length > 0) {
+      showMoreLink = `<div class="col-12 my-1">
+        <a href="#" class="show-comments" data-index="${index}" style = "color: #2b1055;">Show more comments</a>
+      </div>`;
+    }
+  }
+  document.getElementById(`commentWrapper${index}`).innerHTML = commentWrapper + showMoreLink;
+});
+
+// Add event listener to handle showing remaining comments
+document.querySelectorAll('.show-comments').forEach(anchor => {
+  anchor.addEventListener('click', function(event) {
+    event.preventDefault();
+    const dataIndex = this.getAttribute('data-index');
+    const remainingCommentWrapper = questionComments[questions[dataIndex].id]
+      .slice(1)
+      .map(comment => {
+        const imagePath = comment.profiles.image_path;
+        const userName = comment.profiles.username;
+        return `
+          <div class="col-2 my-1">
+            <img
+              src="${itemsImageUrl + imagePath}"
+              class="block my-2 border border-dark border-2 rounded-circle d-flex align-items-center"
+              width="40px"
+              height="40px"
+            />
+          </div>
+          <div class="col-10 my-1">
+            <div class="card">
+              <div class="card-body p-0 ms-2">
+                <b>${userName}</b>
+                <p>${comment.comment_text}</p>
+              </div>
+            </div>
+          </div>`;
+      }).join('');
+    // Append the remaining comments
+    document.getElementById(`commentWrapper${dataIndex}`).insertAdjacentHTML('beforeend', remainingCommentWrapper);
+    // Remove the anchor link
+    this.parentElement.removeChild(this);
+  });
+});
+
+  
+}
+
+
+
+
 
 form_modal.onsubmit = async (e) => {
   e.preventDefault();
@@ -449,7 +595,7 @@ const submit_heart = async (e) => {
 
 window.onload = function() {
   setTimeout(function() {
-    window.scrollTo(0, 2);
+    window.scrollTo(0, 50);
   },1500); 
 }
 
